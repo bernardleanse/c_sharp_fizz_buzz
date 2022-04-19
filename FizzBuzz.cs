89ce0bf20bb936c2c84af0497c75b8d908ff91ec
Bernard Leanse <bernardleanse96@gmail.com>
2022-04-19 19:07:45 +0100

    string prints

---
+++ FizzBuzz.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FizzBuzzSpace
+{
+  public class FizzBuzz
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello This is FizzBuzz main function");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning a bit of C#
+
+So far I've got a basic string printing! Getting used to the syntax!
